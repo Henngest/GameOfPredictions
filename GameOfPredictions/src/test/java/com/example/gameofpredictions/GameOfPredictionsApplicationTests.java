d65f747ab9814ce76f789666941730065415506c
Filip Danilovski <filip_danilovski@hotmail.com>
2022-10-11 23:29:21 +0200

    Initial commit

---
+++ GameOfPredictions/src/test/java/com/example/gameofpredictions/GameOfPredictionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gameofpredictions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameOfPredictionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
