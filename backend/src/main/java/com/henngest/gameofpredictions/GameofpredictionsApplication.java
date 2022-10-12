9e5aedcd62668624e10f758957eadf331f3caa54
Filip Danilovski <filip_danilovski@hotmail.com>
2022-10-12 20:26:40 +0200

    Initial commit

---
+++ backend/src/main/java/com/henngest/gameofpredictions/GameofpredictionsApplication.java
@@ -0,0 +1,13 @@
+package com.henngest.gameofpredictions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameofpredictionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameofpredictionsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/henngest/gameofpredictions/GameofpredictionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henngest.gameofpredictions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameofpredictionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
