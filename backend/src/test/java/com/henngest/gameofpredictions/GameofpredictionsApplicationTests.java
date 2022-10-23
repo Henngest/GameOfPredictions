b4a5d9d69d5057cce4d0fb5e0dc8631eddf31226
Aleksandar Andevski <a.andevski@gmail.com>
2022-10-23 19:51:58 +0100

    Autoformat

--- backend/src/test/java/com/henngest/gameofpredictions/GameofpredictionsApplicationTests.java
+++ backend/src/test/java/com/henngest/gameofpredictions/GameofpredictionsApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GameofpredictionsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
