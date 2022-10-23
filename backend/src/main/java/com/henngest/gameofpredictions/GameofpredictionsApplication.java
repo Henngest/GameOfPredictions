f9af71be3e4aa65bcb26773853002c23f4806cfc
Aleksandar Andevski <a.andevski@gmail.com>
2022-10-23 19:28:51 +0100

    Autoformat files

--- backend/src/main/java/com/henngest/gameofpredictions/GameofpredictionsApplication.java
+++ backend/src/main/java/com/henngest/gameofpredictions/GameofpredictionsApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GameofpredictionsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GameofpredictionsApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(GameofpredictionsApplication.class, args);
+  }
 }
