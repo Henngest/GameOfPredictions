44ae2e8b22fb8c292d064f46a004d55b79f6adc5
filip.danilovski <filip.danilovski@sorsix.com>
2023-07-19 13:30:54 +0200

    Initialize backend project

---
+++ backend/gop-backend/src/test/kotlin/com/sorsix/gopbackend/GopBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sorsix.gopbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GopBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
