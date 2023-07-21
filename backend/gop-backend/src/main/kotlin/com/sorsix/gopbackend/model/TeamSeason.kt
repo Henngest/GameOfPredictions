e8c7e30bbc2f9305119509cf530837d53c133615
Kiril Kovacheski <kirekovacheski@gmail.com>
2023-07-21 12:49:20 +0200

    Added corresponding entity for TeamSeason relationship

---
+++ backend/gop-backend/src/main/kotlin/com/sorsix/gopbackend/model/TeamSeason.kt
@@ -0,0 +1,17 @@
+package com.sorsix.gopbackend.model
+
+import javax.persistence.*
+
+@Entity
+@Table(name="team_seasons")
+data class TeamSeason(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+    @ManyToOne
+    @JoinColumn(name="team_id")
+    val team: Team,
+    @ManyToOne
+    @JoinColumn(name="season_id")
+    val season: Season
+)
