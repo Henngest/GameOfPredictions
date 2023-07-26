6e1eb7a708818c848179a70cef3f536c68673419
filip.danilovski <filip.danilovski@sorsix.com>
2023-07-26 13:44:13 +0200

    Added Reactive Forms

--- frontend/gop-frontend/src/app/login/login.component.ts
+++ frontend/gop-frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AbstractControlOptions, FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm: FormGroup = new FormGroup({});
+  isSubmitted = false
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    const controlOptions: AbstractControlOptions = { updateOn: 'submit' };
+
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    }, controlOptions);
+  }
+
+  submit(){
+    this.isSubmitted = true;
+    if(this.loginForm.invalid)
+      return;
+  }
 }
--- frontend/gop-frontend/src/app/register/register.component.ts
+++ frontend/gop-frontend/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {countryList} from "../CountryList";
+import {AbstractControlOptions, FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -9,5 +10,29 @@ import {countryList} from "../CountryList";
 export class RegisterComponent {
 
   countryList = countryList;
+  registerForm: FormGroup = new FormGroup({});
+  isSubmitted = false;
+  passwordsMatch = true;
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit(): void{
+    const controlOptions: AbstractControlOptions = { updateOn: 'submit' };
+
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+      country: ['', Validators.required]
+    }, controlOptions);
+  }
+
+  submit(){
+    this.isSubmitted = true;
+    this.passwordsMatch = this.registerForm.get('password')?.value == this.registerForm.get('confirmPassword')?.value;
+    if(this.registerForm.invalid)
+      return;
+  }
 
 }
