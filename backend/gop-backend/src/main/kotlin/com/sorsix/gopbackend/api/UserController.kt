2637808bc183d02684f9a02a79f47c2ea7173747
Kiril Kovacheski <kirekovacheski@gmail.com>
2023-08-01 13:10:05 +0200

    Added endpoint for retrieving leaderboard of users by rating

--- backend/gop-backend/src/main/kotlin/com/sorsix/gopbackend/api/UserController.kt
+++ backend/gop-backend/src/main/kotlin/com/sorsix/gopbackend/api/UserController.kt
@@ -3,22 +3,43 @@ package com.sorsix.gopbackend.api
 import com.sorsix.gopbackend.model.User
 import com.sorsix.gopbackend.repository.UserRepository
 import com.sorsix.gopbackend.service.UserService
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.awt.print.Pageable
 
 @RestController
-@RequestMapping("/api/profile")
-class UserController(val userRepository: UserRepository,
-val userService: UserService) {
+@RequestMapping("/api/users")
+class UserController(
+    val userRepository: UserRepository,
+    val userService: UserService
+) {
 
     @GetMapping
     fun showUserDetails(@RequestParam username: String): User? =
-            this.userRepository.findByUsername(username)
+        this.userRepository.findByUsername(username)
 
     @PutMapping("/edit")
     fun editUserDetails(@RequestParam username: String, @RequestParam country: String): User =
-            this.userService.editUserDetails(username, country)
+        this.userService.editUserDetails(username, country)
+
+    @GetMapping("/getAllUsers")
+    fun getAllUsersSortedByRating(
+        @RequestParam page: Int,
+        @RequestParam size: Int,
+        @RequestParam(required = false) sortDirection: String?
+    ): ResponseEntity<Page<User>> =
+        if (sortDirection != null) {
+            val sortingCriteria = Sort.by(Sort.Direction.fromString(sortDirection), "rating")
+            ResponseEntity.ok(this.userRepository.findAll(PageRequest.of(page, size, sortingCriteria)))
+        } else {
+            val sortingCriteria = Sort.by(Sort.Direction.DESC, "rating")
+            ResponseEntity.ok(this.userRepository.findAll(PageRequest.of(page, size, sortingCriteria)))
+        }
 }
