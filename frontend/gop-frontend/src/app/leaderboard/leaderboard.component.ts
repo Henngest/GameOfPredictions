d3ca5006868e1cba1961da6c3a0b5a337cf682a5
Kiril Kovacheski <kirekovacheski@gmail.com>
2023-08-01 14:24:20 +0200

    Finished leaderboard component with pagination

--- frontend/gop-frontend/src/app/leaderboard/leaderboard.component.ts
+++ frontend/gop-frontend/src/app/leaderboard/leaderboard.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
-import {User} from "../interfaces/user";
+import {Component, OnInit} from '@angular/core';
+import {Page} from "../interfaces/page";
+import {LeaderboardService} from "../leaderboard.service";
+import {ActivatedRoute} from "@angular/router";
+import {mergeMap} from "rxjs";
+import {DecimalPipe} from "@angular/common";
+import {faSpinner} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-leaderboard',
   templateUrl: './leaderboard.component.html',
-  styleUrls: ['./leaderboard.component.css']
+  styleUrls: ['./leaderboard.component.css'],
+  providers: [DecimalPipe]
 })
-export class LeaderboardComponent {
+export class LeaderboardComponent implements OnInit {
 
-  users: User[] | undefined;
+  page: Page | undefined;
+  loading: boolean = true;
+  faSpinner = faSpinner;
+
+  constructor(private leaderboardService: LeaderboardService,
+              private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.route.queryParamMap.pipe(
+      mergeMap(params => {
+        const page = params.has('page') ? +params.get('page')!! : 0;
+        const size = params.has('size') ? +params.get('size')!! : 3;
+
+        return this.leaderboardService.getUsersSortedByRating(page, size);
+      })
+    ).subscribe(value => {
+      this.page = value;
+      this.loading = false;
+    })
+  }
+
 }
