13fc046337acd7616837fe487ac950d624169053
Kiril Kovacheski <kirekovacheski@gmail.com>
2023-08-10 10:29:13 +0200

    Added on delete and on update rules in sql init script

--- backend/gop-backend/src/main/resources/db/migration/V1__init.sql
+++ backend/gop-backend/src/main/resources/db/migration/V1__init.sql
@@ -8,13 +8,13 @@ create table season
 (
     id             bigserial primary key,
     name           text not null,
-    competition_id bigint references competition
+    competition_id bigint references competition on delete cascade on update cascade
 );
 create table matchday
 (
     id              bigserial primary key,
     matchday_number bigint,
-    season_id       bigint references season,
+    season_id       bigint references season on delete cascade on update cascade,
     start_time      timestamp not null,
     is_finished     boolean default false
 );
@@ -26,9 +26,9 @@ create table team
 create table fixture
 (
     id                        bigserial primary key,
-    home_team_id              name references team,
-    away_team_id              name references team,
-    matchday_id               bigint references matchday,
+    home_team_id              name references team on delete cascade on update cascade,
+    away_team_id              name references team on delete cascade on update cascade,
+    matchday_id               bigint references matchday on delete cascade on update cascade,
     home_team_win_coefficient real      not null,
     away_team_win_coefficient real      not null,
     draw_coefficient          real      not null,
@@ -48,13 +48,13 @@ create table users
 create table prediction
 (
     id                bigserial primary key,
-    user_id           text references users,
-    fixture_id        bigint references fixture,
+    user_id           text references users on delete cascade on update cascade,
+    fixture_id        bigint references fixture on delete cascade on update cascade,
     predicted_outcome text not null
 );
 create table team_seasons
 (
     id        bigserial primary key,
-    team_id   text references team,
-    season_id bigint references season
+    team_id   text references team on delete cascade on update cascade,
+    season_id bigint references season on delete cascade on update cascade
 );
