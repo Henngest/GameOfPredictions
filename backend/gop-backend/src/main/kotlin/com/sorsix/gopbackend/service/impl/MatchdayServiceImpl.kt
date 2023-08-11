eee72bad4c77db677e99e78c0a25d60c4abf0137
Kiril Kovacheski <kirekovacheski@gmail.com>
2023-08-11 11:53:56 +0200

    Extracted code in separate function to improve readability

--- backend/gop-backend/src/main/kotlin/com/sorsix/gopbackend/service/impl/MatchdayServiceImpl.kt
+++ backend/gop-backend/src/main/kotlin/com/sorsix/gopbackend/service/impl/MatchdayServiceImpl.kt
@@ -2,12 +2,15 @@ package com.sorsix.gopbackend.service.impl
 
 import com.sorsix.gopbackend.model.Fixture
 import com.sorsix.gopbackend.model.Matchday
+import com.sorsix.gopbackend.model.Season
 import com.sorsix.gopbackend.model.Team
 import com.sorsix.gopbackend.model.enumerations.Outcome
 import com.sorsix.gopbackend.model.exceptions.FixtureDoesNotExistException
 import com.sorsix.gopbackend.model.exceptions.MatchdayDoesNotExistException
 import com.sorsix.gopbackend.model.exceptions.SeasonDoesNotExistException
 import com.sorsix.gopbackend.model.exceptions.TeamDoesNotExistException
+import com.sorsix.gopbackend.model.partial.ParsedFixturePartial
+import com.sorsix.gopbackend.model.partial.ParsedMatchday
 import com.sorsix.gopbackend.repository.*
 import com.sorsix.gopbackend.service.MatchdayService
 import com.sorsix.gopbackend.service.PredictionService
@@ -49,6 +52,39 @@ class MatchdayServiceImpl(
         }
     }
 
+    private fun createMatchday(parsedMatchday: ParsedMatchday, season: Season): Matchday {
+        return parsedMatchday.run {
+            Matchday(
+                id = 0,
+                matchdayNumber = matchdayNumber,
+                startTime = startTime,
+                season = season,
+                fixtures = null
+            )
+        }
+    }
+
+    private fun createFixture(parsedFixture: ParsedFixturePartial, matchday: Matchday, teams: HashMap<String, Team>): Fixture {
+        return parsedFixture.run {
+            val homeTeam = getTeam(teams, homeTeam)
+            val awayTeam = getTeam(teams, awayTeam)
+
+            Fixture(
+                id = 0,
+                homeTeam = homeTeam,
+                awayTeam = awayTeam,
+                startTime = startTime,
+                homeTeamWinCoefficient = homeTeamWinCoefficient,
+                awayTeamWinCoefficient = awayTeamWinCoefficient,
+                drawCoefficient = drawCoefficient,
+                matchday = matchday,
+                homeTeamGoals = null,
+                awayTeamGoals = null,
+                outcome = null
+            )
+        }
+    }
+
     override fun importMatchdaysFromFile(seasonId: Long, file: InputStream) {
         val parsedMatchdays = this.fileParserUtil.parseMatchdaysFromFile(file)
         val season = this.seasonRepository.findByIdOrNull(seasonId)
@@ -56,41 +92,22 @@ class MatchdayServiceImpl(
 
         val teams = HashMap<String, Team>()
         parsedMatchdays.forEach { parsedMatchday ->
-            val matchday = with(parsedMatchday) {
-                Matchday(
-                    id = 0,
-                    matchdayNumber = matchdayNumber,
-                    startTime = startTime,
-                    season = season,
-                    fixtures = null
-                )
-            }
-
+            val matchday = createMatchday(parsedMatchday, season)
             this.matchdayRepository.save(matchday)
 
             parsedMatchday.fixtures?.forEach { parsedFixture ->
-                val homeTeam = getTeam(teams, parsedFixture.homeTeam)
-                val awayTeam = getTeam(teams, parsedFixture.awayTeam)
-                val fixture = with(parsedFixture) {
-                    Fixture(
-                        id = 0,
-                        homeTeam = homeTeam,
-                        awayTeam = awayTeam,
-                        startTime = startTime,
-                        homeTeamWinCoefficient = homeTeamWinCoefficient,
-                        awayTeamWinCoefficient = awayTeamWinCoefficient,
-                        drawCoefficient = drawCoefficient,
-                        matchday = matchday,
-                        homeTeamGoals = null,
-                        awayTeamGoals = null,
-                        outcome = null
-                    )
-                }
-
+                val fixture = createFixture(parsedFixture, matchday, teams)
                 this.fixtureRepository.save(fixture)
             }
         }
     }
+
+    private fun getCoefficientFromOutcome(outcome: Outcome, fixture: Fixture): Double =
+        when (outcome) {
+            Outcome.HOME_TEAM_WIN -> fixture.homeTeamWinCoefficient
+            Outcome.AWAY_TEAM_WIN -> fixture.awayTeamWinCoefficient
+            else -> fixture.drawCoefficient
+        }
 
     override fun importMatchdayResultsFromFile(matchdayId: Long, file: InputStream) {
         val fixtures = this.fileParserUtil.parseFixtureResultsFromFile(file)
@@ -108,11 +125,7 @@ class MatchdayServiceImpl(
                 )
             )
             this.predictionRepository.findAllByFixture(fixture).forEach { it2 ->
-                val coefficient = when (it2.predictedOutcome) {
-                    Outcome.HOME_TEAM_WIN -> fixture.homeTeamWinCoefficient
-                    Outcome.AWAY_TEAM_WIN -> fixture.awayTeamWinCoefficient
-                    else -> fixture.drawCoefficient
-                }
+                val coefficient = getCoefficientFromOutcome(it2.predictedOutcome, fixture)
                 this.predictionService.checkPredictionAndUpdateRating(
                     it2.predictedOutcome,
                     fixture.outcome!!,
